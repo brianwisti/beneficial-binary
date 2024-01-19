95e0fcce90105f04ba757ecb9af45a710183f896
Brian Wisti <brianwisti@pobox.com>
2024-01-18 20:09:28 -0800

    posts are basically rendered; add deflist and mvp css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 import remarkObsidian from 'remark-obsidian';
+import { remarkDefinitionList, defListHastHandlers } from 'remark-definition-list';
 
 // https://astro.build/config
 export default defineConfig({
     markdown: {
-        remarkPlugins: [remarkObsidian],
+        remarkPlugins: [remarkObsidian, remarkDefinitionList],
+        remarkRehype: {
+            handlers: {
+                ...defListHastHandlers,
+            }
+        }
     }
 });
--- scripts/pull.ts
+++ scripts/pull.ts
@@ -9,6 +9,12 @@ import XRegexp from "xregexp";
 
 dotenv.config();
 
+type PullSettings = {
+    vaultPath: Path;
+    contentPath: Path;
+    pullList: string[];
+}
+
 const allowedExtensions = [".md"];
 const wikiLinkPattern = `
     \\[\\[
@@ -27,19 +33,10 @@ const logger = pino({
 
 async function main() {
     logger.info("Pulling content from Obsidian vault...");
-    const vaultPath = new Path(process.env.VAULT_PATH!);
-    const contentPath = new Path(process.env.CONTENT_PATH!);
-    const pullList = process.env.PULL_LIST!
-        .split("\n")
-        .map((line) => line.trim())
-        .filter((line) => line.length > 0);
+    const pullSettings = loadPullSettings();
 
-    logger.info(`Vault path: ${vaultPath}`);
-    logger.info(`Content path: ${contentPath}`);
-    logger.info(`Pull list: ${pullList}`);
-
-    for (const folder of pullList) {
-        await pullDirectory(vaultPath, contentPath, folder);
+    for (const folder of pullSettings.pullList) {
+        await pullDirectory(pullSettings, folder);
     }
 }
 
@@ -108,12 +105,32 @@ async function loadNote(vaultDir: Path, vaultFile: Path) {
     return note;
 }
 
-async function pullDirectory(vaultDir: Path, contentDir: Path, folder: string) {
+function loadPullSettings() {
+    const vaultPath = new Path(process.env.VAULT_PATH!);
+    const contentPath = new Path(process.env.CONTENT_PATH!);
+    const pullList = process.env.PULL_LIST!
+        .split("\n")
+        .map((line) => line.trim())
+        .filter((line) => line.length > 0);
+
+    logger.info(`Vault path: ${vaultPath}`);
+    logger.info(`Content path: ${contentPath}`);
+    logger.info(`Pull list: ${pullList}`);
+
+    return {
+        vaultPath,
+        contentPath,
+        pullList,
+    };
+}
+
+async function pullDirectory(pullSettings: PullSettings, folder: string) {
+    const { vaultPath } = pullSettings;
     let count = 0;
-    const folderDir = vaultDir.child(folder);
+    const folderDir = vaultPath.child(folder);
 
     for await (const file of folderDir.list({ recursive: true })) {
-        const filePulled = await pullFile(vaultDir, contentDir, file);
+        const filePulled = await pullFile(pullSettings, file);
 
         if (filePulled) {
             count++;
@@ -122,16 +139,19 @@ async function pullDirectory(vaultDir: Path, contentDir: Path, folder: string) {
     logger.info(`Pulled ${count} files from ${folder}`);
 }
 
-async function pullFile(vaultDir: Path, contentDir: Path, vaultFile: Path) {
+async function pullFile(pullSettings: PullSettings, vaultFile: Path) {
     if (!await canPullFile(vaultFile)) {
         return false;
     }
 
-    const note = await loadNote(vaultDir, vaultFile);
+    const { vaultPath, contentPath } = pullSettings;
 
-    let contentElements = vaultDir.relative(vaultFile).toArray();
+    const note = await loadNote(vaultPath, vaultFile);
+
+    let contentElements = vaultPath.relative(vaultFile).toArray();
     let slugParts = contentElements
         .map((part) => slugify(part, { lower: true }));
+
     if (note.data.slug) {
         // If the note has a slug, use that instead of the last part of the path.
         slugParts[slugParts.length - 1] = `${note.data.slug}.md`;
@@ -139,7 +159,7 @@ async function pullFile(vaultDir: Path, contentDir: Path, vaultFile: Path) {
 
     const contentSlugPath = slugParts.join("/");
     logger.debug(`Relative path: ${contentSlugPath}`);
-    const contentFile = contentDir.child(contentSlugPath);
+    const contentFile = contentPath.child(contentSlugPath);
 
     // if (await contentFile.exists()) {
     //     logger.debug(`Skipping ${vaultFile} because ${contentFile} already exists.`);
